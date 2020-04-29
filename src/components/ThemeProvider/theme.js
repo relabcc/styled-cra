af7f30ddd307b300fff7566665dc1b419c267e8f
Renddi Yeh <renddi@relab.cc>
2020-04-29 16:35:45 +0800

    updated

--- src/components/ThemeProvider/theme.js
+++ src/components/ThemeProvider/theme.js
@@ -1,16 +1,34 @@
 import merge from 'lodash/merge';
 import range from 'lodash/range';
+import last from 'lodash/last';
 import mapValues from 'lodash/mapValues';
 
 import colors from 'open-color/open-color.json';
+
+import memorize from '../../utils/memorize'
 
 const baseFontSize = 16
 const emToPx = (em) => `${em * baseFontSize}px`;
 
 export const breakpoints = [22, 36, 48, 62, 90, 120].map(emToPx);
 export const containerWidth = [22, 36, 46, 58].map(emToPx);
-export const responsive = (mobile, desktop) => [mobile, null, null, desktop];
-export const responsiveIndex = 3;
+export const responsiveIndex = [
+  3, // mobile
+  // 4, // laptop
+]
+
+export const responsive = memorize((...args) => breakpoints.map((_, i) => {
+  if (args.length <= 1) return args[0]
+  let r = last(args)
+  let index = 0
+  while (r === last(args) && index < responsiveIndex.length) {
+    if (i < responsiveIndex[index]) r = args[index]
+    index += 1
+  }
+  return r
+}));
+
+export const mobileOrDesktop = responsive
 
 const generateFade = (r, g, b) => range(10, 100, 10)
   .reduce((fade, opacity) => merge(fade, { [opacity]: `rgba(${[r, g, b, opacity / 100].join()})` }), {});
