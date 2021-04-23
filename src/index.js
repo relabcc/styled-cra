0d7b216cc4aa736f243b5a375a9e709b31aa93fe
Renddi Yeh <renddi@relab.cc>
2021-04-23 15:01:44 +0800

    upgraded

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import MediaProvider from './contexts/mediaQuery/MediaProvider'
 import HeaderProvider from './contexts/header/Provider'
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <ThemeProvider>
@@ -23,4 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
